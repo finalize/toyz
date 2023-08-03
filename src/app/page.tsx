bd001ce30dcb97ec3fdb4f73780ea6ca54fd2905
finalize <shgysd@gmail.com>
2023-08-03 21:49:51 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ const getBaseUrl = () => {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  return "http://localhost:3000"
+  return "http://127.0.0.1:3000"
 }
 
 async function getData() {
-  const res = await axios.get(`${getBaseUrl()}/api/shops/yahoo`)
-  console.log(res.data.data, "**********")
+  const res = await fetch(`${getBaseUrl()}/api/shops/yahoo`)
+  console.log(getBaseUrl(), "**********")
 
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
-  return res.data.data
+  return await res.json()
 }
 
 export default async function Home() {
-  const data = await getData()
+  const { data } = await getData()
 
   console.log(data, "data")
 
