567c945919bd0987c71aa8a25d4c80210eef401a
finalize <shgysd@gmail.com>
2023-08-03 22:00:26 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,23 +10,24 @@ const getBaseUrl = () => {
 }
 
 async function getData() {
-  const res = await fetch(`${getBaseUrl()}/api/shops/yahoo`)
-  console.log(getBaseUrl(), "**********")
+  const res = await fetch(`${getBaseUrl()}/api/shops/yahoo`).then(async (v) => {
+    return await v.json()
+  })
 
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
-  return await res.json()
+  return res
 }
 
 export default async function Home() {
-  const { data } = await getData()
+  const data = await getData()
 
-  console.log(data, "data")
+  // console.log(data, "data")
 
   return (
     <main className="">
-      {data?.map((value: string) => (
+      {data?.data.map((value: string) => (
         <img src={value} alt="" />
       ))}
     </main>
