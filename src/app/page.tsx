3bb77a2454c8b7a2711a0f98364c0de54555a6a5
finalize <shgysd@gmail.com>
2023-08-03 20:43:37 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image"
 
+const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
+    return "https://your-production.url"
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  return "http://localhost:3000"
+}
+
 async function getData() {
-  console.log(process.env.NEXT_PUBLIC_API_DOMAIN)
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/shops/yahoo`
-  )
+  console.log(process.env, getBaseUrl())
+  const res = await fetch(`${getBaseUrl()}/api/shops/yahoo`)
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
